e40720784bbfa5c12bd26f684c555e0864540899
Kristine <kteich88@gmail.com>
2016-09-19 17:10:24 -0400

    Added hashes with conversion values, and calculations for conversion rates.

--- currency_converter.rb
+++ currency_converter.rb
@@ -4,6 +4,22 @@ class UnknownCurrencyCodeError < StandardError
 end
 
 class CurrencyConverter
+  attr_accessor :current, :converted
+
+  def initialize()
+    @conversion_rates_usd = { "USD": 1.0, "EUR": 0.89472, "JPY": 101.866 }
+    @conversion_rates_eur = { "EUR": 1.0, "USD": 1.11766, "JPY": 113.855 }
+    @conversion_rates_jpy = { "JPY": 1.0, "EUR": 0.00879, "USD": 0.00982 }
+  end
+
+  def conversion(current, converted)
+    @current = current
+    @converted = converted
+    converted = Currency.new(current.amount * @conversion_rates_usd["USD"], "EUR", "JPY")
+    converted = Currency.new(current.amount * @conversion_rates_usd["EUR"], "USD", "JPY")
+    converted = Currency.new(current.amount * @conversion_rates_usd["JPY"], "EUR", "USD")
+  end
+
 
 
 
