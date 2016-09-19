4f3a38a510ba167965bcb35b990bfb320b551402
Kristine <kteich88@gmail.com>
2016-09-19 14:34:20 -0400

    Added :currency_symbol back as attribute

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,11 +1,11 @@
 class Currency
   attr_reader :currency_symbol, :currency_amount
 
-  def initiaize(currency_amount, currency_code)
+  def initiaize(currency_symbol, currency_amount, currency_code)
+    @currency_symbol = currency_symbol
     @currency_amount = currency_amount
     @currency_code = currency_code
   end
 
   
-
 end
