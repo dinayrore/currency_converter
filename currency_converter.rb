b1aeabc38067381d404a9f66ca28d79df508a8fa
Kristine <kteich88@gmail.com>
2016-09-19 14:07:06 -0400

    Added attributes to class Currency

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,7 +1,8 @@
 class Currency
-  attr_reader
+  attr_reader :
 
   def initiaize(currency_symbol, currency_amount)
-
+    @currency_symbol = currency_symbol
+    @currency_amount = currency_amount
   end
 end
